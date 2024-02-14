6d8d1135f5225d6c3ecf77397ddfa4eebdca3fa1
Grant Ramsay <seapagan@gmail.com>
2024-02-14 09:23:49 +0000

    update the SettingsSchemaError exception with a custom message
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- simple_toml_settings/exceptions.py
+++ simple_toml_settings/exceptions.py
@@ -16,6 +16,15 @@ class SettingsNotFoundError(SettingsError):
 class SettingsSchemaError(SettingsError):
     """The settings file schema does not match the required level."""
 
+    def __init__(self, expected: str, found: str) -> None:
+        """Define a custom response for this Exception."""
+        self.expected = expected
+        self.found = found
+        super().__init__(
+            f"Schema version mismatch: Expected {self.expected}, "
+            f"found {self.found} in file."
+        )
+
 
 # temporary alias for backwards compatibility
 SettingsNotFound = SettingsNotFoundError
